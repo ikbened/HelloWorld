c8b4ad06d15ecc9cc4ba46011e9e4ae3e39a8d79
ikbened <ed.hilkman@spronq.com>
2018-01-12 16:26:09 +0100

    Initial commit

---
+++ src/main/java/com/spronq/HelloWorldServlet.java
@@ -0,0 +1,13 @@
+package java.com.spronq;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from HelloWorldServlet");
+    }
+}
